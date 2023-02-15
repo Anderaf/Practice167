3fabc995f8af9272249cb92da1f93f008a8cd0e8
Anderaf <kuldperea@gmail.com>
2023-02-15 21:59:16 +1000

    Добавьте файлы проекта.

---
+++ Practice167.Tests/CalculatorTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Practice167;
+
+namespace Practice167.Tests
+{
+    internal class CalculatorTests
+    {
+        public void Addition_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+        }
+    }
+}
